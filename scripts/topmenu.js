ef942f654b76beedef4208b67a55b6bc81602870
Ritsuki Goto <develop@mizucoffee.net>
2019-01-08 03:06:32 +0900

    [add] script

---
+++ scripts/topmenu.js
@@ -0,0 +1,52 @@
+// =============================
+//   Yuki-Skin
+//        scripts/top-menu.js
+// =============================
+//                  Mizucoffee
+
+let camera
+
+// 描画開始前に呼ばれる
+module.exports.onStart = (scene) => {
+  console.log('start')
+  camera = new BABYLON.ArcRotateCamera("Camera", 0, Math.PI /3 , 10, new BABYLON.Vector3.Zero(), scene);
+
+  new BABYLON.HemisphericLight(
+    'light1',
+    new BABYLON.Vector3(0, 1, 0),
+    scene
+  )
+
+  const sphere = BABYLON.MeshBuilder.CreateSphere(
+    'sphere',
+    { segments: 16, diameter: 2 },
+    scene
+  )
+
+  sphere.position.y = 1
+
+  BABYLON.MeshBuilder.CreateGround(
+    'ground1',
+    { height: 6, width: 6, subdivisions: 2 },
+    scene
+  )
+}
+
+let alpha = 0
+
+// 描画更新時呼ばれる
+module.exports.onTick = (scene) => {
+  alpha += 0.01
+  if (alpha >= Math.Pi) alpha = -Math.PI
+  camera.alpha = alpha
+}
+
+// キープレス時に呼ばれる
+module.exports.onKeyDown = () => {
+  console.log("onKeyDown")
+}
+
+// 描画終了時に呼ばれる
+module.exports.onFinished = () => {
+  console.log("onFinished")
+}
